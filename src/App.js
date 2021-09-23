97fb188439529d40b46b1ac6b238e3907cb3da9c
VedantJ-Lejhro <vedant.jaiswal@lejhro.com>
2021-09-23 10:39:27 +0530

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+     <div className="navBarA">
+     <Navbar />
+     </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
